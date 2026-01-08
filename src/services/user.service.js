ca96371d75c859099b723b26dc69a490d9bd38c1
Francisco Madera <franciscomaderanatera@gmail.com>
2026-01-08 17:11:24 -0400

    feat: list user con role

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,7 +1,41 @@
 import { userRepository } from "../repositories/user.repository.js";
+import { adminRepository } from "../repositories/admin.repository.js";
+import { driverRepository } from "../repositories/driver.repository.js";
+import { parentRepository } from "../repositories/parent.repository.js";
 
 export const userService = {
   async listUsers() {
-    return await userRepository.listUsers();
+    const users = await userRepository.listUsers();
+
+    return Promise.all(
+      users.map(async (user) => {
+        const parentInfo = await parentRepository.findByUserId(user.user_id);
+        const driverInfo = await driverRepository.findByUserId(user.user_id);
+        const adminInfo = await adminRepository.findByUserId(user.user_id);
+
+        if (parentInfo)
+          return {
+            ...user,
+            ...parentInfo,
+            role: "parent",
+          };
+
+        if (driverInfo)
+          return {
+            ...user,
+            ...driverInfo,
+            role: "driver",
+          };
+
+        if (adminInfo)
+          return {
+            ...user,
+            ...adminInfo,
+            role: "admin",
+          };
+
+        return { ...user };
+      })
+    );
   },
 };
